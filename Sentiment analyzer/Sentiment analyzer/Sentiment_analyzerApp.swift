e3c4bae4999bff5aa21df49e053de857483f05cc
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-24 21:43:04 +0200

    added a new xcode project folder

---
+++ Sentiment analyzer/Sentiment analyzer/Sentiment_analyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sentiment_analyzerApp.swift
+//  Sentiment analyzer
+//
+//  Created by Antonio Palomba on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Sentiment_analyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
