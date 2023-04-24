f56a1855eddcf7874b6dead2fd68c86e4698063b
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-24 21:59:16 +0200

    Added basic UI, and a function running the analyze Sentiment feature

--- Sentiment analyzer/Sentiment analyzer/ContentView.swift
+++ Sentiment analyzer/Sentiment analyzer/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+// This is needed due use the NLTagger()
+import NaturalLanguage
 
 struct ContentView: View {
+    @State private var inputText: String = ""
+
+    private var score: String {
+        return analyzeSentiment(for: inputText)
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Sentiment Analyzer")
+
+            TextField("Enter Text", text: $inputText)
+
+            Text("Score")
         }
         .padding()
+    }
+
+    private let tagger = NLTagger(tagSchemes: [.sentimentScore])
+
+    private func analyzeSentiment(for stringToAnalyze: String) -> String {
+
+        tagger.string = stringToAnalyze
+        ///In thi case we want a tuple storing :
+        /// - stringToAnalyze.startIndex refers to the starting character of the sting
+        /// - unit is what we want to analyse we can choose both a word or a whole paragraph
+        /// - scheme is the type of analysis that we want to run
+        let (sentimentScore, _) = tagger.tag(
+            at: stringToAnalyze.startIndex,
+            unit: .paragraph,
+            scheme: .sentimentScore
+        )
+
+        return sentimentScore?.rawValue ?? ""
     }
 }
 
